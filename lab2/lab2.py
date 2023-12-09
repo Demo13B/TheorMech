03d802ba1d02a51b1568f36ccfce79bf8511635f
TRSalikhov <trsalikhov@mai.education>
2023-12-09 12:56:39 +0300

    Plate is now dynamically drawn depending on alpha angle

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import sympy as sp
+from mpmath import mp
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import math
@@ -9,6 +10,12 @@ def Rot(X, Y, Alpha):
     RX = X * np.cos(Alpha) - Y * np.sin(Alpha)
     RY = X * np.sin(Alpha) + Y * np.cos(Alpha)
     return RX, RY
+
+
+def S(Y0, Z0, s, angle):
+    MY = Y0 + s * mp.sin(angle)
+    MZ = Z0 + s * mp.cos(angle)
+    return MY, MZ
 
 
 # Условия
@@ -21,25 +28,28 @@ l = 3
 v0 = 0
 w0 = math.pi / 6
 
+# Рассчет размеров пластины
+height = 3
+length = height * math.tan(alpha)
+print(length)
+
 # Рассчет времени
 Steps = 1000
 t_fin = 20
 t = np.linspace(0, t_fin, Steps)
 
-# Координаты phi(t)
+# Координаты phi(t), s(t)
 Phi = 2 * math.pi * t
-s = 2 * np.sin(t)
+s = np.sin(math.pi * t)
 
 # Задание параметов для отрисовки пластины
 Plate_X_Start = np.array([0, 0, 0, 0, 0])
-Plate_Y_Start = np.array([-1, 1, 1, -1, -1])
-Plate_Z_Start = np.array([1, 1, 2, 2, 1])
+Plate_Y_Start = np.array(
+    [-length / 2, length / 2, length / 2, -length/2, -length/2])
+Plate_Z_Start = np.array([1, 1, height + 1, height + 1, 1])
 
 Plate_X = np.arange(Steps * 5)
 Plate_X = Plate_X.reshape((Steps, 5))
-
-Channel_X = np.arange(Steps * 2)
-Channel_X = Channel_X.reshape((Steps, 2))
 
 Plate_X = np.zeros_like(Plate_X, float)
 Plate_Y = np.zeros_like(Plate_X, float)
@@ -60,13 +70,17 @@ for i in range(1, Steps):
 Pin_X = [0, 0]
 Pin_Y = [0, 0]
 Pin_Zl = [0, 1]
-Pin_Zc = [1, 2]
-Pin_Zu = [2, 3]
+Pin_Zc = [1, height + 1]
+Pin_Zu = [height + 1, height + 2]
 
 # Задаеник положения канал
 Channel_X_Start = np.array([0, 0])
-Channel_Y_Start = np.array([-1, 1])
-Channel_Z_Start = np.array([1, 2])
+Channel_Y_Start = np.array([-length/2, length/2])
+Channel_Z_Start = np.array([1, height + 1])
+
+Channel_X = np.arange(Steps * 2)
+Channel_X = Channel_X.reshape((Steps, 2))
+
 
 Channel_X = np.zeros_like(Channel_X, float)
 Channel_Y = np.zeros_like(Channel_X, float)
@@ -88,23 +102,32 @@ Point_X = np.arange(Steps, dtype=float)
 Point_Y = np.arange(Steps, dtype=float)
 Point_Z = np.arange(Steps, dtype=float)
 
+Point_X = np.zeros_like(Point_X, float)
+Point_Y = np.zeros_like(Point_Y, float)
+Point_Z = np.zeros_like(Point_Z, float)
+
 Point_X[0] = 0
 Point_Y[0] = 0
-Point_Z[0] = 1.5
+Point_Z[0] = height / 2 + 1
 
 # Рассчет положения точки
 for i in range(1, Steps):
-    A = Rot(Point_X[0], Point_Y[0], Phi[i])
-    Point_X[i] = np.array(A[0])
-    Point_Y[i] = np.array(A[1])
-    Point_Z[i] = Point_Z[0]
+    A = S(Point_Y[0], Point_Z[0], s[i], alpha)
+    Point_Y[i] += A[0]
+    Point_Z[i] += A[1]
+
+    B = Rot(Point_X[i], Point_Y[i], Phi[i])
+    Point_X[i] = np.array(B[0])
+    Point_Y[i] = np.array(B[1])
+    Point_Z[i] = Point_Z[i]
 
 
 # Создание модели
 fig = plt.figure(figsize=[15, 7])
 ax = fig.add_subplot(1, 1, 1, projection='3d')
 ax.axis('equal')
-ax.set(xlim=[-4, 4], ylim=[-4, 4], zlim=[0, 4])
+ax.set(xlim=[- (height + 2), height + 2],
+       ylim=[- (height + 2), height + 2], zlim=[0, height + 2])
 
 # Отрисовка штифта
 ax.plot(Pin_X, Pin_Y, Pin_Zl, color='black')
@@ -132,7 +155,7 @@ def anima(i):
     Point.set_data(Point_X[i], Point_Y[i])
     Point.set_3d_properties(Point_Z[i])
 
-    return [Drawed_Plate, Channel, Point]
+    return [Point]
 
 
 anim = FuncAnimation(fig, anima, frames=len(t), interval=20, repeat=False)
